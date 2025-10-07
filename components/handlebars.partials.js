3bede42b23bf6b2c17d7c3856fb9f4e701b85f49
stvp-qld <stvp-qld@users.noreply.github.com>
2025-10-07 05:01:28 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-175-consistent-fluid-containers/dist REV:5c0bf37c8b2ffa9757b3d8269f76cf481bf412bc
    
    Updates some .container-fluid classes to .container-full, for consistent UI gutters

--- components/handlebars.partials.js
+++ components/handlebars.partials.js
@@ -2,52 +2,46 @@
 /** THIS IS A GENERATED FILE **/
 
 import accordion from "../components/bs5/accordion/accordion.hbs?raw";
-import backToTop from "../components/bs5/backToTop/backToTop.hbs?raw";
 import banner from "../components/bs5/banner/banner.hbs?raw";
 import blockquote from "../components/bs5/blockquote/blockquote.hbs?raw";
 import breadcrumbs from "../components/bs5/breadcrumbs/breadcrumbs.hbs?raw";
+import breadcrumbsWrapper from "../components/bs5/breadcumbsWrapper/breadcrumbsWrapper.hbs?raw";
 import button from "../components/bs5/button/button.hbs?raw";
 import callToAction from "../components/bs5/callToAction/callToAction.hbs?raw";
 import callout from "../components/bs5/callout/callout.hbs?raw";
 import card from "../components/bs5/card/card.hbs?raw";
-import containerLayout from "../components/bs5/containerLayout/containerLayout.hbs?raw";
 import contentFooter from "../components/bs5/contentFooter/contentFooter.hbs?raw";
+import contentFooterWrapper from "../components/bs5/contentFooterWrapper/contentFooterWrapper.hbs?raw";
+import contentWrapper from "../components/bs5/contentWrapper/contentWrapper.hbs?raw";
 import correctincorrect from "../components/bs5/correctincorrect/correctincorrect.hbs?raw";
 import dateinput from "../components/bs5/dateinput/dateinput.hbs?raw";
-import directionLinks from "../components/bs5/directionLinks/directionLinks.hbs?raw";
 import footer from "../components/bs5/footer/footer.hbs?raw";
 import footerForgov from "../components/bs5/footer/footerForgov.hbs?raw";
 import formcheck from "../components/bs5/formcheck/formcheck.hbs?raw";
+import fullPage from "../components/bs5/fullPageWrapper/fullPage.hbs?raw";
 import globalAlert from "../components/bs5/globalAlert/globalAlert.hbs?raw";
 import head from "../components/bs5/head/head.hbs?raw";
 import header from "../components/bs5/header/header.hbs?raw";
 import inpageAlert from "../components/bs5/inpageAlert/inpageAlert.hbs?raw";
 import inpagenav from "../components/bs5/inpagenav/inpagenav.hbs?raw";
-import linkColumns from "../components/bs5/linkColumns/linkColumns.hbs?raw";
+import mainContainerWrapper from "../components/bs5/mainContainerWrapper/mainContainerWrapper.hbs?raw";
 import metaDcTerms from "../components/bs5/metaDcTerms/metaDcTerms.hbs?raw";
 import metaOpenGraph from "../components/bs5/metaOpenGraph/metaOpenGraph.hbs?raw";
 import modal from "../components/bs5/modal/modal.hbs?raw";
 import navbar from "../components/bs5/navbar/navbar.hbs?raw";
 import pagination from "../components/bs5/pagination/pagination.hbs?raw";
-import promotionalPanel from "../components/bs5/promotionalPanel/promotionalPanel.hbs?raw";
 import quickexit from "../components/bs5/quickexit/quickexit.hbs?raw";
 import searchInput from "../components/bs5/searchInput/searchInput.hbs?raw";
 import select from "../components/bs5/select/select.hbs?raw";
 import sidenav from "../components/bs5/sidenav/sidenav.hbs?raw";
+import sidenavWrapper from "../components/bs5/sidenavWrapper/sidenavWrapper.hbs?raw";
 import spinner from "../components/bs5/spinner/spinner.hbs?raw";
 import table from "../components/bs5/table/table.hbs?raw";
-import tabs from "../components/bs5/tabs/tabs.hbs?raw";
 import tag from "../components/bs5/tag/tag.hbs?raw";
 import textarea from "../components/bs5/textarea/textarea.hbs?raw";
 import textbox from "../components/bs5/textbox/textbox.hbs?raw";
 import typography from "../components/bs5/typography/typography.hbs?raw";
 import video from "../components/bs5/video/video.hbs?raw";
-import breadcrumbsWrapper from "../components/bs5/wrappers/breadcrumbsWrapper/breadcrumbsWrapper.hbs?raw";
-import contentFooterWrapper from "../components/bs5/wrappers/contentFooterWrapper/contentFooterWrapper.hbs?raw";
-import contentWrapper from "../components/bs5/wrappers/contentWrapper/contentWrapper.hbs?raw";
-import fullPage from "../components/bs5/wrappers/fullPageWrapper/fullPage.hbs?raw";
-import mainContainerWrapper from "../components/bs5/wrappers/mainContainerWrapper/mainContainerWrapper.hbs?raw";
-import sidenavWrapper from "../components/bs5/wrappers/sidenavWrapper/sidenavWrapper.hbs?raw";
 
 
 /**
@@ -57,52 +51,46 @@ import sidenavWrapper from "../components/bs5/wrappers/sidenavWrapper/sidenavWra
  */
 export default function handlebarsPartials(handlebars) {
   handlebars.registerPartial("accordion", accordion);
-  handlebars.registerPartial("backToTop", backToTop);
   handlebars.registerPartial("banner", banner);
   handlebars.registerPartial("blockquote", blockquote);
   handlebars.registerPartial("breadcrumbs", breadcrumbs);
+  handlebars.registerPartial("breadcrumbsWrapper", breadcrumbsWrapper);
   handlebars.registerPartial("button", button);
   handlebars.registerPartial("callToAction", callToAction);
   handlebars.registerPartial("callout", callout);
   handlebars.registerPartial("card", card);
-  handlebars.registerPartial("containerLayout", containerLayout);
   handlebars.registerPartial("contentFooter", contentFooter);
+  handlebars.registerPartial("contentFooterWrapper", contentFooterWrapper);
+  handlebars.registerPartial("contentWrapper", contentWrapper);
   handlebars.registerPartial("correctincorrect", correctincorrect);
   handlebars.registerPartial("dateinput", dateinput);
-  handlebars.registerPartial("directionLinks", directionLinks);
   handlebars.registerPartial("footer", footer);
   handlebars.registerPartial("footerForgov", footerForgov);
   handlebars.registerPartial("formcheck", formcheck);
+  handlebars.registerPartial("fullPage", fullPage);
   handlebars.registerPartial("globalAlert", globalAlert);
   handlebars.registerPartial("head", head);
   handlebars.registerPartial("header", header);
   handlebars.registerPartial("inpageAlert", inpageAlert);
   handlebars.registerPartial("inpagenav", inpagenav);
-  handlebars.registerPartial("linkColumns", linkColumns);
+  handlebars.registerPartial("mainContainerWrapper", mainContainerWrapper);
   handlebars.registerPartial("metaDcTerms", metaDcTerms);
   handlebars.registerPartial("metaOpenGraph", metaOpenGraph);
   handlebars.registerPartial("modal", modal);
   handlebars.registerPartial("navbar", navbar);
   handlebars.registerPartial("pagination", pagination);
-  handlebars.registerPartial("promotionalPanel", promotionalPanel);
   handlebars.registerPartial("quickexit", quickexit);
   handlebars.registerPartial("searchInput", searchInput);
   handlebars.registerPartial("select", select);
   handlebars.registerPartial("sidenav", sidenav);
+  handlebars.registerPartial("sidenavWrapper", sidenavWrapper);
   handlebars.registerPartial("spinner", spinner);
   handlebars.registerPartial("table", table);
-  handlebars.registerPartial("tabs", tabs);
   handlebars.registerPartial("tag", tag);
   handlebars.registerPartial("textarea", textarea);
   handlebars.registerPartial("textbox", textbox);
   handlebars.registerPartial("typography", typography);
   handlebars.registerPartial("video", video);
-  handlebars.registerPartial("breadcrumbsWrapper", breadcrumbsWrapper);
-  handlebars.registerPartial("contentFooterWrapper", contentFooterWrapper);
-  handlebars.registerPartial("contentWrapper", contentWrapper);
-  handlebars.registerPartial("fullPage", fullPage);
-  handlebars.registerPartial("mainContainerWrapper", mainContainerWrapper);
-  handlebars.registerPartial("sidenavWrapper", sidenavWrapper);
 
 }
 if(typeof(Handlebars) !== 'undefined') {
