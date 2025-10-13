a3b4e2e98752bb0dae2eec0b18353e01949024ed
stevelynagh <stevelynagh@users.noreply.github.com>
2025-10-13 00:31:08 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-306_correct_scss_errors/dist REV:50c67d230561292c2f873d08a4790e122042e45e
    
    rebuild handlebars partials

--- assets/js/handlebars.helpers.bundle.js
+++ assets/js/handlebars.helpers.bundle.js
@@ -825,7 +825,7 @@
   </section>
   {{/each }}
 </div>`;var R=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.3","branch":"QGDS-306_correct_scss_errors","tag":"","commit":"02889d3ec1d33b86d4c96a76276cb9c1a01c3523","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.4","branch":"QGDS-306_correct_scss_errors","tag":"","commit":"50c67d230561292c2f873d08a4790e122042e45e","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
