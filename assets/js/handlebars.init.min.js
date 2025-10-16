640152102c6f4b784bd5cfdfcad3a56d5682f359
stvp-qld <stvp-qld@users.noreply.github.com>
2025-10-16 23:03:31 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-317-breadcrumb-focus-outline/dist REV:d5ca2491bb8663d9a9c8229c60c7fe82d65d8209
    
    chore: Dependabot YML update

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -825,7 +825,7 @@
   </section>
   {{/each }}
 </div>`;var R=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.6","branch":"QGDS-317-breadcrumb-focus-outline","tag":"","commit":"00083d0ed30b08449e46572395cdc5f2813e7bea","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.6","branch":"QGDS-317-breadcrumb-focus-outline","tag":"","commit":"d5ca2491bb8663d9a9c8229c60c7fe82d65d8209","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
