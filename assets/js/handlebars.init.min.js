00e0183efb0f9448e525e82d5c3be68659cb20d7
SenChung <SenChung@users.noreply.github.com>
2025-10-21 00:55:21 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-325-update-all-icons-to-use-the-qgds-sprite-icons/dist REV:4f747964d1313433cbb7e275bcec9ee5c9dd391c
    
    Fix: removed the css span and icon from scss

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -825,7 +825,7 @@
   </section>
   {{/each }}
 </div>`;var R=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.6","branch":"QGDS-325-update-all-icons-to-use-the-qgds-sprite-icons","tag":"","commit":"f10dd901df3c95711b7a893b67ad1697c1a87e7f","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.6","branch":"QGDS-325-update-all-icons-to-use-the-qgds-sprite-icons","tag":"","commit":"4f747964d1313433cbb7e275bcec9ee5c9dd391c","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
