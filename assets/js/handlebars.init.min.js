43702cb26f192a7a0affbac7b87c0e450a6da521
longhssq <longhssq@users.noreply.github.com>
2025-10-21 04:48:26 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-324-bugfix-links-double-outlines/dist REV:083d5829c45d65ac043fcbef0585dcd5ba9f41cc
    
    style: remove box shadow from nav-link focus-visible

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -825,7 +825,7 @@
   </section>
   {{/each }}
 </div>`;var R=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.7","branch":"QGDS-324-bugfix-links-double-outlines","tag":"","commit":"4fb6d7151101967b51a92f4ca98339f546ebc473","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.7","branch":"QGDS-324-bugfix-links-double-outlines","tag":"","commit":"083d5829c45d65ac043fcbef0585dcd5ba9f41cc","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
