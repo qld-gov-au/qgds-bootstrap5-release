bb8ec28bcd66c676fac1bfbcd2375935ba19be00
longhssq <longhssq@users.noreply.github.com>
2025-10-28 23:35:41 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-303-image-caption-design-updat/dist REV:2b4128481f82d1881090dcd4f4ae6e17db37ecdd
    
    style: change hbs

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1089,7 +1089,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.8","branch":"QGDS-303-image-caption-design-updat","tag":"","commit":"19267c32930e0660a5fcd212673c7e55a3143047","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.8","branch":"QGDS-303-image-caption-design-updat","tag":"","commit":"2b4128481f82d1881090dcd4f4ae6e17db37ecdd","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
@@ -1397,8 +1397,6 @@ cdn := PROD|STAGING|BETA|TEST|DEV|???
       </figcaption>
     </figure>
   {{/ifAny}}
-  {{else}}
-  {{> imageTag}}
 {{/if}}`;var B=`<div class="alert {{variantClass}} {{customClass}}" role="alert" 
     {{#if alertType}}aria-label="{{alertType}}"{{/if}}>
     {{#if heading}}
