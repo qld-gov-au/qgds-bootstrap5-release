7d37b142ed9a807460b204bea59919f84b13716c
stvp-qld <stvp-qld@users.noreply.github.com>
2025-11-03 02:10:25 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-351-fix-focus-outline-colour/dist REV:22a98f7caaf809e643fc4f375c49a0e67019a141
    
    Fix: focus mixin nesting; storybook background args;

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1112,7 +1112,7 @@
   </section>
   {{/each }}
 </div>`;var I=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.9","branch":"QGDS-351-fix-focus-outline-colour","tag":"","commit":"6d31bf32403bde26e58d05aa6fe35b40be629186","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.9","branch":"QGDS-351-fix-focus-outline-colour","tag":"","commit":"22a98f7caaf809e643fc4f375c49a0e67019a141","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
