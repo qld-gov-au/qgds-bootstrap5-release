fa8b4062e92adf87bb69774be7064271253e5b9a
stevelynagh <stevelynagh@users.noreply.github.com>
2025-11-05 22:46:47 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-337-icons-color/dist REV:ebc99d46fec772e5a5efab1c6b39986c89167e69
    
    remove commented code

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1117,7 +1117,7 @@
   </section>
   {{/each }}
 </div>`;var I=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.11","branch":"QGDS-337-icons-color","tag":"","commit":"eb141b142da177798f15580358f1ab34d3ed0202","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.11","branch":"QGDS-337-icons-color","tag":"","commit":"ebc99d46fec772e5a5efab1c6b39986c89167e69","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
