1848dc9ea9da2615dc0d680442209b4854eebf43
stevelynagh <stevelynagh@users.noreply.github.com>
2025-11-05 02:27:42 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-252-bs5-form-validation-states/dist REV:3a9cef99827a8f8950c17230809e0e1c04de9cef
    
    fix: input focus border color, hint text styles.

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1117,7 +1117,7 @@
   </section>
   {{/each }}
 </div>`;var I=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.10","branch":"QGDS-252-bs5-form-validation-states","tag":"","commit":"fc95c4553fd3df39af5b04607612075f6a6d453e","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.10","branch":"QGDS-252-bs5-form-validation-states","tag":"","commit":"3a9cef99827a8f8950c17230809e0e1c04de9cef","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
