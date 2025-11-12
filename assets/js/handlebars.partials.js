0cfc1268a24cfef874f02b8aa5856b7debc35a41
SenChung <SenChung@users.noreply.github.com>
2025-11-12 06:21:31 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-338-search-input-component-change-lookup-behaviour-design-test-fix/dist REV:4843f38f04fa507fa4936dae40bad7dcb380d7cf
    
    Fix: Updated Search Input dark theme

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1117,7 +1117,7 @@
   </section>
   {{/each }}
 </div>`;var I=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.12","branch":"QGDS-338-search-input-component-change-lookup-behaviour-design-test-fix","tag":"","commit":"098845722e70c743ec746ca71efb0c050413a87a","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.0.12","branch":"QGDS-338-search-input-component-change-lookup-behaviour-design-test-fix","tag":"","commit":"4843f38f04fa507fa4936dae40bad7dcb380d7cf","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
