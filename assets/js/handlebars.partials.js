8335264bd5f9e27699b5fb254931c50372ac4ddd
stevelynagh <stevelynagh@users.noreply.github.com>
2025-11-17 07:35:32 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:chai-6.2.0/dist REV:a9b871518be3dfa3021b75a13f06210d76363cd8
    
    Merge branch 'develop' into dependabot/npm_and_yarn/develop/chai-6.2.0

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1117,7 +1117,7 @@
   </section>
   {{/each }}
 </div>`;var I=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"HEAD","tag":"v2.1.0","commit":"1610f625e3e2ba5c9420ba3ef0ebb53ed35820f6","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"dependabot/npm_and_yarn/develop/chai-6.2.0","tag":"","commit":"a9b871518be3dfa3021b75a13f06210d76363cd8","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
