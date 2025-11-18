a343105bd487bbdaf4be7091626d5c6a4b3bf7eb
duttonw <duttonw@users.noreply.github.com>
2025-11-18 02:12:15 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:f18f626aa7e90bfc6ea235eadf291bf047d29fdb
    
    Merge pull request #913 from qld-gov-au/nov_patching
    
    Nov patching - Dependabot rollup including fixes from library changes

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"develop","tag":"","commit":"be22bce98812236913718f04c1db998331f1c737","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"develop","tag":"","commit":"f18f626aa7e90bfc6ea235eadf291bf047d29fdb","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
