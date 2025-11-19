449b00ecf9a6181017ee45e78c5077ae1058fea9
SenChung <SenChung@users.noreply.github.com>
2025-11-19 00:18:49 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-353-prefers-reduced-motion-state-for-accessibility/dist REV:4eac25c689f5d30abeaf6120d0459f553d3fa63c
    
    Fix: exported the reduced motion to its own file

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"QGDS-353-prefers-reduced-motion-state-for-accessibility","tag":"","commit":"c34ea1571d8ba0c1e2d6b7b506c642943e9d4d9b","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"QGDS-353-prefers-reduced-motion-state-for-accessibility","tag":"","commit":"4eac25c689f5d30abeaf6120d0459f553d3fa63c","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
