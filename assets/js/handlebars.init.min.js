4e46a9a0cb2ae86a438e24341ac4a130c802c6d0
stevelynagh <stevelynagh@users.noreply.github.com>
2025-11-19 23:07:16 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:53fc3395ce8e41368cd7a81cc701b187fcd1192a
    
    Merge pull request #930 from qld-gov-au/dependabot/npm_and_yarn/develop/rimraf-6.1.2
    
    chore(deps-dev): bump rimraf from 6.1.0 to 6.1.2

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"develop","tag":"","commit":"873f627171eca869fcce818add804e2a3d1c386f","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.0","branch":"develop","tag":"","commit":"53fc3395ce8e41368cd7a81cc701b187fcd1192a","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
