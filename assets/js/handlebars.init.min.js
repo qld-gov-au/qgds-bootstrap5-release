c1f36e9c01478f84e7db5e8cea7c39fe2aa57303
SenChung <SenChung@users.noreply.github.com>
2025-12-01 02:06:56 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-520-Fix-global-alert-padding-and-margins/dist REV:883d852943bae2cdc80d0ce85f141c10f47d8d0b
    
    Fix: Removed container-full from global-alert and updated the test script

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1080,7 +1080,7 @@
 
 <div class="global-alert-include">
   {{#each alertItems}}
-  <section role="region" class="global-alert container-full d-none {{variant}}" data-variant="{{variant}}"
+  <section role="region" class="global-alert d-none {{variant}}" data-variant="{{variant}}"
     {{#if ariaLabel}} aria-label="{{ ariaLabel }}"
     {{~else ifCond variant "==" "global-alert-critical"}} aria-label="Critical alert"
     {{~else ifCond variant "==" "global-alert-info"}} aria-label="Informational alert"
@@ -1119,7 +1119,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.2","branch":"QGDS-520-Fix-global-alert-padding-and-margins","tag":"","commit":"b584c4c6ed8f185e56f12bb2785a5c9e457d1845","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.2","branch":"QGDS-520-Fix-global-alert-padding-and-margins","tag":"","commit":"883d852943bae2cdc80d0ce85f141c10f47d8d0b","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
