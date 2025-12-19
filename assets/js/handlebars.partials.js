f52e775018c7d90b9b9f2ac1da18fb87733ff0c7
stevelynagh <stevelynagh@users.noreply.github.com>
2025-12-19 01:50:14 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:96339397d207ff378f8d97d9adef18ddbcccd295
    
    Merge pull request #965 from qld-gov-au/QGDS-573-incorrect-padding-searchinput
    
    QGDS-573 Search Input Incorrect padding

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1119,7 +1119,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.5","branch":"develop","tag":"","commit":"f35a79c357d2fc71bc6b6aca3428b73b9639d112","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.5","branch":"develop","tag":"","commit":"96339397d207ff378f8d97d9adef18ddbcccd295","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
