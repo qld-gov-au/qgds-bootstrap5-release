21db9b304d3b7ff5c6362c05dc5eec6369ac046f
stvp-qld <stvp-qld@users.noreply.github.com>
2025-12-22 23:47:40 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:2c611e283a2f9f868940d66c5dc0e459a6684694
    
    Merge pull request #966 from qld-gov-au/QGDS-571-webkit-flags-for-chrome-v102
    
    Feature (css): Adds browserlist, postcss and autoprefix

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1119,7 +1119,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.6","branch":"develop","tag":"v2.1.6","commit":"808c456d6a9e74b77f5534ffe7bd8c1c3c27d354","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.6","branch":"develop","tag":"","commit":"2c611e283a2f9f868940d66c5dc0e459a6684694","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
