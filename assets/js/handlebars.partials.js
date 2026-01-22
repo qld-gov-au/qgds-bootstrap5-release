e749ef9fc8d34a179c4e3b958bc4b315fe8b6dcc
stvp-qld <stvp-qld@users.noreply.github.com>
2026-01-22 03:51:52 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:fd6023bec53eae60901e2a7b90528d481c7c4a6e
    
    Merge pull request #983 from qld-gov-au/QGDS-582-mobile-email-addresses-dont-fit-on-screen
    
    QGDS-582: Fix: Word wrap and break for long email link in footer

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1119,7 +1119,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.8","branch":"develop","tag":"","commit":"27d988ac8f18576bb4d6ff898e222a2c75ca439e","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.8","branch":"develop","tag":"","commit":"fd6023bec53eae60901e2a7b90528d481c7c4a6e","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
