53350c4a0fd5af1d051e65f2246852efc0260822
stvp-qld <stvp-qld@users.noreply.github.com>
2026-01-27 02:38:26 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:lodash-4.17.23/dist REV:7a8a23a727796fb2ca1acb8710ea448422532dab
    
    Merge branch 'develop' into dependabot/npm_and_yarn/lodash-4.17.23

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1119,7 +1119,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.9","branch":"HEAD","tag":"v2.1.9","commit":"228f45b9dea4dd6674147c93a086038ac167746f","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.9","branch":"dependabot/npm_and_yarn/lodash-4.17.23","tag":"","commit":"7a8a23a727796fb2ca1acb8710ea448422532dab","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
