556b84643fc3462210745d27f48e3f506a6fe472
stvp-qld <stvp-qld@users.noreply.github.com>
2026-01-28 02:38:37 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:e7bf8c5c9b6316582d8275f2665f539d639ee186
    
    Merge pull request #933 from qld-gov-au/dependabot/npm_and_yarn/develop/vitest-6580560de6
    
    chore(deps-dev): bump the vitest group with 2 updates

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1119,7 +1119,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.9","branch":"develop","tag":"","commit":"23071be641510cdd11eaa9388062c5e75cfa344b","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.9","branch":"develop","tag":"","commit":"e7bf8c5c9b6316582d8275f2665f539d639ee186","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
