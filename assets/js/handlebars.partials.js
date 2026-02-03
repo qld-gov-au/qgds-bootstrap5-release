748e94e19281e6377866a95ceaa481bd857265ce
stvp-qld <stvp-qld@users.noreply.github.com>
2026-02-03 23:25:51 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:be4841c1b6f6f5a5d757c74011f92d9c37fae967
    
    Merge pull request #1002 from qld-gov-au/dependabot/npm_and_yarn/develop/jsdom-28.0.0
    
    chore(deps-dev): bump jsdom from 27.4.0 to 28.0.0

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1119,7 +1119,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.10","branch":"develop","tag":"","commit":"3c8d92ba416e388ce259cf44228597e40b07e4bb","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.10","branch":"develop","tag":"","commit":"be4841c1b6f6f5a5d757c74011f92d9c37fae967","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
