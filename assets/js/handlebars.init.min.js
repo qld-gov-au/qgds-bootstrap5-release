3972c2e690cf0640b8c18ce5849a05d20ccb786a
stvp-qld <stvp-qld@users.noreply.github.com>
2026-02-06 03:49:58 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:9cc5234bc7ad6d8cb3e98019550d2c1c6b62f0cb
    
    Merge pull request #1005 from qld-gov-au/dependabot/npm_and_yarn/develop/storybook-addon-tag-badges-3.0.6
    
    chore(deps-dev): bump storybook-addon-tag-badges from 3.0.4 to 3.0.6

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.10","branch":"develop","tag":"","commit":"198b3222ae892f7793c0c383ff25d7d99566cdb6","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.10","branch":"develop","tag":"","commit":"9cc5234bc7ad6d8cb3e98019550d2c1c6b62f0cb","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
