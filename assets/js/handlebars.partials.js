47819d61db3e0000382bd712e9a2d6bf2a639353
SenChung <SenChung@users.noreply.github.com>
2026-02-12 03:52:58 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-583-Search-Input-Icon-Fix/dist REV:6f0cfae1659ee275416d71930766e6dd3d4de8d9
    
    Merge branch 'develop' into QGDS-583-Search-Input-Icon-Fix

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.11","branch":"QGDS-583-Search-Input-Icon-Fix","tag":"","commit":"e613f6bd545199a2124993a5d3f3e6ebf8dace9b","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.11","branch":"QGDS-583-Search-Input-Icon-Fix","tag":"","commit":"6f0cfae1659ee275416d71930766e6dd3d4de8d9","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
