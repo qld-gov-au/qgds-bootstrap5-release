80324e36b54eac2f14dc9652d771e4e909040dc5
stvp-qld <stvp-qld@users.noreply.github.com>
2026-02-12 03:55:45 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:391de91a5d7c7e1ab9452c199557042c1780273e
    
    Merge pull request #1014 from qld-gov-au/fix-esbuild-eslint-plugin-dependency
    
    chore(eslint): Remove redundant plugin esbuild-plugin-eslint

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.11","branch":"develop","tag":"","commit":"c9955a2367291e2c198b78ca35efca86a6208f22","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.11","branch":"develop","tag":"","commit":"391de91a5d7c7e1ab9452c199557042c1780273e","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
