3cede6a91be031cdfe267282ed40250b3ed64992
stvp-qld <stvp-qld@users.noreply.github.com>
2026-02-17 23:28:41 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:35d3b6adab6a391157cabc7f1f7d8ba4c065f294
    
    Merge pull request #1020 from qld-gov-au/dependabot/npm_and_yarn/develop/storybook-b27cc0dac8
    
    chore(deps-dev): bump the storybook group with 8 updates

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"abba3ba6f338a6409438db70535d13de472fe2fc","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"35d3b6adab6a391157cabc7f1f7d8ba4c065f294","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
