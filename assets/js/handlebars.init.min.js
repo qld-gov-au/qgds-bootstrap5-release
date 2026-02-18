7dca143ad859873bb1c3830dff50b2f118068d73
stvp-qld <stvp-qld@users.noreply.github.com>
2026-02-18 00:49:10 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:2abe63093d1474e14c58eedc06b5819078399bbd
    
    Merge pull request #915 from qld-gov-au/dependabot/npm_and_yarn/develop/autoprefixer-10.4.22
    
    chore(deps-dev): bump autoprefixer from 10.4.21 to 10.4.22

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"09fb4eebfd5f7d1ce8723630eb2c89af52b49b17","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"2abe63093d1474e14c58eedc06b5819078399bbd","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
