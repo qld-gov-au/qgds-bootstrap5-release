a12c9186599c0f195a2367caea626b42afbbf9f3
longhssq <longhssq@users.noreply.github.com>
2026-02-19 07:32:39 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-720-details-bootstrap/dist REV:0190bddc054aef59062a4a600481c46bc3e5bbdd
    
    feat: tidy up styles and storybook

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -743,13 +743,13 @@
     </div>
 </div>
 `;var T=`<!-- QGDS Component: Details -->
-<details class="qld-details{{#if open}} open{{/if}}">
-    <summary class="qld-details-summary">
-        {{summaryText}}
-    </summary>
-    <div class="qld-details-content">
-        {{{content}}}
-    </div>
+<details class="qld-details{{#if size}} qld-details-{{size}}{{/if}}{{#if open}} open{{/if}}">
+	<summary class="qld-details-summary">
+		{{summaryText}}
+	</summary>
+	<div class="qld-details-content">
+		{{{content}}}
+	</div>
 </details>
 `;var A=`{{! valid class values are: up, down, left, right}}
 <a class="qld-dir-link {{class}}" 
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"QGDS-720-details-bootstrap","tag":"","commit":"4e2045a66b5c5a86a0c3340b7f3d1679fd39df13","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"QGDS-720-details-bootstrap","tag":"","commit":"0190bddc054aef59062a4a600481c46bc3e5bbdd","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
