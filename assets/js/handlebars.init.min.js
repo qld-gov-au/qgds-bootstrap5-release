8d9c7a94f4e2907163aa84c4f830c1bbe4728662
stvp-qld <stvp-qld@users.noreply.github.com>
2026-04-01 03:12:39 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:main/dist REV:0837b20180c98de83e9d2bc6955447d6780748b4
    
    Merge pull request #1063 from qld-gov-au/dependabot/npm_and_yarn/flatted-3.4.2
    
    chore(deps-dev): bump flatted from 3.3.3 to 3.4.2

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.16","branch":"main","tag":"","commit":"d32fe9aeab77bd377fd25b732323eb56a3015959","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.16","branch":"main","tag":"","commit":"0837b20180c98de83e9d2bc6955447d6780748b4","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
