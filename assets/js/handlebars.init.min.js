a444a85ea28d420b3290ce6c5085a381a1fd64ad
stvp-qld <stvp-qld@users.noreply.github.com>
2026-04-13 22:48:08 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:bb197ddc879a20db2d4c86871ae93c8a6381b33a
    
    Merge pull request #1089 from qld-gov-au/dependabot/npm_and_yarn/develop/globals-17.5.0
    
    chore(deps-dev): bump globals from 17.4.0 to 17.5.0

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"0d9dee3809bacd27cebfec3dd7932bb190e00362","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"bb197ddc879a20db2d4c86871ae93c8a6381b33a","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
