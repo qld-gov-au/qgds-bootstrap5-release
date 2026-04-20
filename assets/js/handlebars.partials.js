4ceb6f3923fa774c035c24dc3a7983e5cf511fa4
qld-gov-au-qgds-ServiceAccount <qld-gov-au-qgds-ServiceAccount@users.noreply.github.com>
2026-04-20 02:07:02 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:v2.1.19/dist REV:731612952424872bfadbe60c8d4fbb02926c6029
    
    2.1.19

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1118,7 +1118,7 @@
   </section>
   {{/each }}
 </div>`;var E=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.19","branch":"main","tag":"v2.1.19","commit":"731612952424872bfadbe60c8d4fbb02926c6029","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.19","branch":"HEAD","tag":"v2.1.19","commit":"731612952424872bfadbe60c8d4fbb02926c6029","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
