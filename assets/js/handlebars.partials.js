97a51bffdce11d4d1e9bc8968b2c719ec859c2bf
teenafrancis25 <teenafrancis25@users.noreply.github.com>
2026-04-22 04:02:35 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-824-Details-Design-QA-defect/dist REV:a06d9e4c70bd7ff9464772ec2d2e7d6e2b9deecd
    
    fix: used line height function

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"QGDS-824-Details-Design-QA-defect","tag":"","commit":"d876b47ee45d91a86ae516570556a17ba1178418","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"QGDS-824-Details-Design-QA-defect","tag":"","commit":"a06d9e4c70bd7ff9464772ec2d2e7d6e2b9deecd","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
