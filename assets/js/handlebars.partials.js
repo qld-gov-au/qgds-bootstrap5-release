0be8e238c5e62cfdfa9972f5ee3b007e6cfd8fe8
teenafrancis25 <teenafrancis25@users.noreply.github.com>
2026-04-27 02:57:48 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:2ff1041dceb9fc57f8e82c192bc1a1736bd80b5d
    
    Merge pull request #1102 from qld-gov-au/QGDS-824-Details-Design-QA-defect
    
    QGDS-824 details design qa defect

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -745,7 +745,8 @@
 `;var T=`<!-- QGDS Component: Details -->
 <details class="qld-details{{#if size}} qld-details-{{size}}{{/if}}"{{#if open}} open{{/if}}>
 	<summary class="qld-details-summary">
-		{{summaryText}}
+		<span class="qld-details-summary-text">{{summaryText}}</span>
+		
 	</summary>
 	<div class="qld-details-content">
 		{{{content}}}
@@ -1127,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"b19eda7a88d24722efe06fcba732efd79f666bcc","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.12","branch":"develop","tag":"","commit":"2ff1041dceb9fc57f8e82c192bc1a1736bd80b5d","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
