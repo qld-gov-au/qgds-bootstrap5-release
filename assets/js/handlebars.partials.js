cb0043c552c7c70ed18014caac39cd89c7a65369
stvp-qld <stvp-qld@users.noreply.github.com>
2026-06-01 07:56:26 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:4df0f938ba0d5e2dcdb2ffd355ba857cae836271
    
    Merge pull request #1131 from qld-gov-au/dependabot/npm_and_yarn/develop/node-html-parser-7.1.0
    
    chore(deps-dev): bump node-html-parser from 7.0.2 to 7.1.0

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.20","branch":"develop","tag":"","commit":"f03587593000db63a9355dd570f7b6cf1c92b22a","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.20","branch":"develop","tag":"","commit":"4df0f938ba0d5e2dcdb2ffd355ba857cae836271","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
