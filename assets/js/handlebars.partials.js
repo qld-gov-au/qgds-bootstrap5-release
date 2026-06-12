237a86153c2852f564cbccec1b4025efa054d97e
stvp-qld <stvp-qld@users.noreply.github.com>
2026-06-12 02:03:33 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:f101e8b58d6f4634ec56ece498fa2b02a555b68a
    
    Merge pull request #1142 from qld-gov-au/dependabot/npm_and_yarn/develop/npm-run-all2-9.0.1
    
    chore(deps-dev): bump npm-run-all2 from 8.0.4 to 9.0.1

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.22","branch":"develop","tag":"","commit":"e34040f77b614cef9f46fb541a9804ccabe02fb1","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.1.22","branch":"develop","tag":"","commit":"f101e8b58d6f4634ec56ece498fa2b02a555b68a","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
