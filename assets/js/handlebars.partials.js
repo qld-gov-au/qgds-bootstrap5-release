3e701a8df007c19c44917064571fd47b19b3d120
stvp-qld <stvp-qld@users.noreply.github.com>
2026-06-22 04:08:36 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:bc69b28d3141a4266e45755e45e47a0f93dea90c
    
    Merge pull request #1166 from qld-gov-au/dependabot/npm_and_yarn/develop/selenium-webdriver-4.45.0
    
    chore(deps-dev): bump selenium-webdriver from 4.44.0 to 4.45.0

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.2","branch":"develop","tag":"","commit":"1fbbb0928e984a0e53d44f8f17b51447dee6da02","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.2","branch":"develop","tag":"","commit":"bc69b28d3141a4266e45755e45e47a0f93dea90c","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
