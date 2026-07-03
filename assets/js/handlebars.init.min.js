afe4e82e6d6dc811fee6ce747d600fc49fa9eabb
elvishu <elvishu@users.noreply.github.com>
2026-07-03 00:40:03 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:es-develop-dependabot-PR1169/dist REV:e95de808263c2bfa8d4cffb8db2e1e1d6b1fa53e
    
    Merge branch 'develop' of https://github.com/qld-gov-au/qgds-bootstrap5 into es-develop-dependabot-PR1169

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"HEAD","tag":"v2.2.3","commit":"6578f21bfbc2b4f35bf12d7e4764f89dce5b6c94","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"es-develop-dependabot-PR1169","tag":"","commit":"e95de808263c2bfa8d4cffb8db2e1e1d6b1fa53e","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
