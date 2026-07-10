a32d87e548578aebc02b4a55ae60be6303c39a53
stvp-qld <stvp-qld@users.noreply.github.com>
2026-07-10 00:37:00 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:d88d87ae12e05094725e349119bae6648cd805bf
    
    Merge pull request #1194 from qld-gov-au/dependabot/npm_and_yarn/develop/prettier-3.9.5
    
    chore(deps-dev): bump prettier from 3.9.4 to 3.9.5

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"0762c58b6938e30139777f4e230ba78efc2c5d09","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"d88d87ae12e05094725e349119bae6648cd805bf","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
