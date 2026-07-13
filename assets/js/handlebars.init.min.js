e77e695cf4645df7b7fae8553a8b6456c04dafb5
stvp-qld <stvp-qld@users.noreply.github.com>
2026-07-13 23:15:18 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:4cff94e731d9959b8db06ac4510843fd13153ed8
    
    Merge pull request #1196 from qld-gov-au/dependabot/npm_and_yarn/develop/eslint-1f52dd2533
    
    chore(deps-dev): bump the eslint group across 1 directory with 2 updates

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"4d8280a055ac3d88445fb0f8d148515666289305","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"4cff94e731d9959b8db06ac4510843fd13153ed8","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
