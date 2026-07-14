eaa6ae089e39976692582cf81d0c05ad8f82c1d4
stvp-qld <stvp-qld@users.noreply.github.com>
2026-07-14 00:12:51 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:d01c0b824ffdc276b022c3bc3615123b2096f1bc
    
    Merge pull request #1200 from qld-gov-au/dependabot/npm_and_yarn/develop/postcss-8.5.19
    
    chore(deps-dev): bump postcss from 8.5.18 to 8.5.19

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"4cff94e731d9959b8db06ac4510843fd13153ed8","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"d01c0b824ffdc276b022c3bc3615123b2096f1bc","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
