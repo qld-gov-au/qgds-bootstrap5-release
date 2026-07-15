d33976e4d2fa3cc106afddd4be5b480c835ceec8
stvp-qld <stvp-qld@users.noreply.github.com>
2026-07-15 04:54:36 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-1144-consolidated-QA-fix/dist REV:cae23729b094253522557ba3e05ad9be48e613ca
    
    fix(table): QGDS-1138 fix table-dark border color, organise variables

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"QGDS-1144-consolidated-QA-fix","tag":"","commit":"5362e1d93f72c061bc8e04f023f7404ba06ee5e9","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"QGDS-1144-consolidated-QA-fix","tag":"","commit":"cae23729b094253522557ba3e05ad9be48e613ca","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
