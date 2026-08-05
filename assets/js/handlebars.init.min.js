413cad48a3ed3f8f885dd70dcf069c3f516c754f
stvp-qld <stvp-qld@users.noreply.github.com>
2026-08-05 02:59:21 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:main/dist REV:f16f9cad0cd5238ef3d5d11c18179e9d74444b8e
    
    Merge pull request #1239 from qld-gov-au/dependabot/npm_and_yarn/undici-7.29.0
    
    chore(deps-dev): bump undici from 7.28.0 to 7.29.0

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.4","branch":"main","tag":"","commit":"2afc09441496a067edbb1d0e61c587d520dba686","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.4","branch":"main","tag":"","commit":"f16f9cad0cd5238ef3d5d11c18179e9d74444b8e","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
