ef47b14be9e44c78cd12449a6f9303b72a691cda
stvp-qld <stvp-qld@users.noreply.github.com>
2026-08-10 01:36:57 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:a587a61d6f3e73c22b29bb0e4c0e753ac90a3334
    
    chore(deps-dev): bump eslint-plugin-storybook in the eslint group
    
    Bumps the eslint group with 1 update: [eslint-plugin-storybook](https://github.com/storybookjs/storybook/tree/HEAD/code/lib/eslint-plugin).
    
    Updates `eslint-plugin-storybook` from 10.5.6 to 10.5.7
    - [Release notes](https://github.com/storybookjs/storybook/releases)
    - [Changelog](https://github.com/storybookjs/storybook/blob/next/CHANGELOG.md)
    - [Commits](https://github.com/storybookjs/storybook/commits/v10.5.7/code/lib/eslint-plugin)
    
    ---
    updated-dependencies:
    - dependency-name: eslint-plugin-storybook
      dependency-version: 10.5.7
      dependency-type: direct:development
      update-type: version-update:semver-patch
      dependency-group: eslint
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1128,7 +1128,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"284f520fe9cb57cd9d05271d9419fb29e673e0a6","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.2.3","branch":"develop","tag":"","commit":"a587a61d6f3e73c22b29bb0e4c0e753ac90a3334","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
