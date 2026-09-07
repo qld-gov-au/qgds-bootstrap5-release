431fd1b04246f8e6cdb20f102b918a519a282bde
stvp-qld <stvp-qld@users.noreply.github.com>
2026-09-07 23:25:27 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:develop/dist REV:b448f59b77f892e6e2e2daecfcf47fc351325921
    
    chore(deps-dev): bump the vitest group across 1 directory with 2 updates
    
    Bumps the vitest group with 2 updates in the / directory: [@vitest/ui](https://github.com/vitest-dev/vitest/tree/HEAD/packages/ui) and [vitest](https://github.com/vitest-dev/vitest/tree/HEAD/packages/vitest).
    
    Updates `@vitest/ui` from 4.1.11 to 5.0.0
    - [Release notes](https://github.com/vitest-dev/vitest/releases)
    - [Changelog](https://github.com/vitest-dev/vitest/blob/main/docs/releases.md)
    - [Commits](https://github.com/vitest-dev/vitest/commits/v5.0.0/packages/ui)
    
    Updates `vitest` from 4.1.11 to 5.0.0
    - [Release notes](https://github.com/vitest-dev/vitest/releases)
    - [Changelog](https://github.com/vitest-dev/vitest/blob/main/docs/releases.md)
    - [Commits](https://github.com/vitest-dev/vitest/commits/v5.0.0/packages/vitest)
    
    ---
    updated-dependencies:
    - dependency-name: "@vitest/ui"
      dependency-version: 5.0.0
      dependency-type: direct:development
      update-type: version-update:semver-major
      dependency-group: vitest
    - dependency-name: vitest
      dependency-version: 5.0.0
      dependency-type: direct:development
      update-type: version-update:semver-major
      dependency-group: vitest
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"develop","tag":"","commit":"fd1aa3b90cd5ee477119f2cf0a0759b2ae99861e","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"develop","tag":"","commit":"b448f59b77f892e6e2e2daecfcf47fc351325921","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
