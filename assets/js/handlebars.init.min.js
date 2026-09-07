6832852001119a17c52c536d033da64176191810
stvp-qld <stvp-qld@users.noreply.github.com>
2026-09-07 00:38:29 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:align-main-with-v2.4.0/dist REV:d550b37afcfd21de2f42c3409a6c02dfb3e77acb
    
    chore: align source version with v2.4.0 release

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"HEAD","tag":"v2.4.0","commit":"1009b34feaf2fee57a7477e7962529d3a6dc8c4e","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"align-main-with-v2.4.0","tag":"","commit":"d550b37afcfd21de2f42c3409a6c02dfb3e77acb","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
