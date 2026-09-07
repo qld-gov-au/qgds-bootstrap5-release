1b04cef4075ddc1bd0a9312e68e59884139818a2
elvishu <elvishu@users.noreply.github.com>
2026-09-07 07:16:14 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:footer-ui-tests/dist REV:dc849771f23b3274a2638ed448b17ca45e74aba6
    
    Add Footer UI test - including aria labellings

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"main","tag":"","commit":"79aadeaad6f2caa5a05d48ec2ec2d871598955c0","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"footer-ui-tests","tag":"","commit":"dc849771f23b3274a2638ed448b17ca45e74aba6","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
