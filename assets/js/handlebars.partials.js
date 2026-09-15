db7a491279002b9a8a61626a338cb286178d64a5
stevelynagh <stevelynagh@users.noreply.github.com>
2026-09-15 06:56:04 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-1217-content-meaningful-sequence/dist REV:1144b63355fab2d6141f6f45a796b9892c55a278
    
    test: extend timeout for failing test

--- assets/js/handlebars.partials.js
+++ assets/js/handlebars.partials.js
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"QGDS-1217-content-meaningful-sequence","tag":"","commit":"a0a2de8eaca6a935a1ddfa35b511934c897c22d2","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"QGDS-1217-content-meaningful-sequence","tag":"","commit":"1144b63355fab2d6141f6f45a796b9892c55a278","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
