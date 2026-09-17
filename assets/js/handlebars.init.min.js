1adc7e78d068d2351f13f05f5c4c7cfe89859caa
stevelynagh <stevelynagh@users.noreply.github.com>
2026-09-17 00:04:53 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-1217-content-meaningful-sequence/dist REV:60c68f4cf815ce046cf312e82ec457af73b15e55
    
    chore: isolate header interactions stories

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"QGDS-1217-content-meaningful-sequence","tag":"","commit":"d0dab35e2471d1ad0472369da1588d9eee3112cf","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"QGDS-1217-content-meaningful-sequence","tag":"","commit":"60c68f4cf815ce046cf312e82ec457af73b15e55","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
