6343ed8b9ad1143135068080997cbcc358d8d8dc
elvishu <elvishu@users.noreply.github.com>
2026-09-17 07:15:02 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:QGDS-1228--Responsive-font-scaling--A11y/dist REV:decada03985481fcf6e863c6a8e8af74d00ab8f0
    
    fix: typo

--- assets/js/handlebars.init.min.js
+++ assets/js/handlebars.init.min.js
@@ -1127,7 +1127,7 @@
   </section>
   {{/each }}
 </div>`;var _=`
-<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"QGDS-1228--Responsive-font-scaling--A11y","tag":"","commit":"6bab849a0c9608d6baabba7751cf787548b4c52c","majorVersion":"v2"} -->
+<!-- VERSION_DETAILS={"project_id":"@qld-gov-au/qgds-bootstrap5","version":"2.4.0","branch":"QGDS-1228--Responsive-font-scaling--A11y","tag":"","commit":"decada03985481fcf6e863c6a8e8af74d00ab8f0","majorVersion":"v2"} -->
 
 {{! Select environment, used verbatium if not using predefind key
 cdn := PROD|STAGING|BETA|TEST|DEV|???
@@ -1267,7 +1267,6 @@ cdn := PROD|STAGING|BETA|TEST|DEV|???
                         <form 
                         class="{{isdefined assets.siteSearch.customClass "site-search"}}" 
                         action="{{isdefined assets.siteSearch.formAction.url "https://www.qld.gov.au/search"}}"
-                        role="search" 
                         >
                           {{{ searchInput }}}
                         </form>
@@ -2053,10 +2052,10 @@ cdn := PROD|STAGING|BETA|TEST|DEV|???
 </div>`;var n1=`<!-- 
     QGDS Component: Search input 
 -->
-<div class="qld-search-input {{customClass}}">
+<div class="qld-search-input {{customClass}}" role="search" {{#if ariaLabelContainer}}aria-label="{{ ariaLabelContainer }}"{{/if}}>
     <input id="{{ inputID }}" name="{{ inputName }}" class="form-control" type="text" autocomplete="off"
-        aria-label="{{ ariaLabel }}" {{#each tags}} data-{{@key}}="{{this}}" {{/each}} placeholder="{{placeholder}}" />
-    <button class="btn btn-primary" type="{{ buttonType }}" id="{{ buttonID }}" aria-label="{{ buttonLabel }}">
+        aria-label="{{ ariaLabel }}" {{#each tags}} data-{{@key}}="{{this}}" {{/each}} placeholder="{{placeholder}}" role="searchbox" enterkeyhint="search"/>
+    <button class="btn btn-primary" type="{{ buttonType }}" id="{{ buttonID }}" >
         <span class="btn-icon" aria-hidden="true"></span>
         <span class="btn-label">{{ buttonLabel }}</span>
     </button>
